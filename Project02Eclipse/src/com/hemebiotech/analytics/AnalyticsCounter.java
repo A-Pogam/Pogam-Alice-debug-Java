97aeba83c154876cc0720c0e7d806e267aaee69e
A-Pogam <alicepogam59@hotmail.com>
2023-10-29 16:31:08 +0100

    added clean javadoc comments and erased old ones

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,63 +5,93 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class AnalyticsCounter {
-	private ISymptomReader reader; // Instance of ISymptomReader for reading symptoms
-	private ISymptomWriter writer; // Instance of ISymptomWriter for writing symptoms
+/**
+ * AnalyticsCounter class for processing and counting symptoms.
+ */
 
-	// Constructor to initialize the reader and writer
+public class AnalyticsCounter {
+	private ISymptomReader reader;
+	private ISymptomWriter writer;
+
+	/**
+	 * Constructor for initializing the reader and writer.
+	 *
+	 * @param reader The ISymptomReader instance for reading symptoms.
+	 * @param writer The ISymptomWriter instance for writing symptoms.
+	 */
 	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
 		this.reader = reader;
 		this.writer = writer;
 	}
 
-	// Get the list of symptoms from the data source
+	/**
+	 * Get the list of symptoms from the data source.
+	 *
+	 * @return A list of symptoms as strings.
+	 */
 	public List<String> getSymptoms() {
 		return reader.GetSymptoms();
 	}
 
-	// Count the occurrences of each symptom
+	/**
+	 * Count the occurrences of each symptom.
+	 *
+	 * @param symptoms A list of symptoms to count.
+	 * @return A map containing symptoms as keys and their counts as values.
+	 */
 	public Map<String, Integer> countSymptoms(List<String> symptoms) {
-		Map<String, Integer> symptomCounts = new HashMap<>(); // Create a map to store symptom counts
-		for (String symptom : symptoms) { // Iterate through the list of symptoms
-			if (!symptom.isEmpty()) { // Check if the symptom is not empty
+		Map<String, Integer> symptomCounts = new HashMap<>();
+		for (String symptom : symptoms) {
+			if (!symptom.isEmpty()) {
 				symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
-				// Increment the count for the symptom or initialize it to 1
 			}
 		}
-		return symptomCounts; // Return the map of symptom counts
+		return symptomCounts;
 	}
 
-	// Sort the symptoms alphabetically
+	/**
+	 * Sort the symptoms alphabetically.
+	 *
+	 * @param symptoms A map of symptoms and their counts.
+	 * @return A sorted map of symptoms.
+	 */
 	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
-		Map<String, Integer> sortedSymptoms = new TreeMap<>(symptoms); // Create a TreeMap for sorting
-		return sortedSymptoms; // Return the sorted map
+		Map<String, Integer> sortedSymptoms = new TreeMap<>(symptoms);
+		return sortedSymptoms;
 	}
 
-	// Write the symptoms and their counts to the output file
+	/**
+	 * Write the symptoms and their counts to the output file.
+	 *
+	 * @param symptoms A map of symptoms and their counts.
+	 */
 	public void writeSymptoms(Map<String, Integer> symptoms) {
-		writer.writeSymptoms(symptoms); // Delegate the writing to the ISymptomWriter
+		writer.writeSymptoms(symptoms);
 	}
 
-	// Generate the symptom report by orchestrating the steps
+	/**
+	 * Generate the symptom report by orchestrating the steps.
+	 */
 	public void generateReport() {
-		List<String> symptoms = getSymptoms(); // Get the list of symptoms
-		Map<String, Integer> symptomCounts = countSymptoms(symptoms); // Count the occurrences
-		Map<String, Integer> sortedSymptoms = sortSymptoms(symptomCounts); // Sort alphabetically
-		writeSymptoms(sortedSymptoms); // Write the report to the output file
+		List<String> symptoms = getSymptoms();
+		Map<String, Integer> symptomCounts = countSymptoms(symptoms);
+		Map<String, Integer> sortedSymptoms = sortSymptoms(symptomCounts);
+		writeSymptoms(sortedSymptoms);
 		System.out.println("Report generated and written to the output file.");
 	}
 
-	// The entry point of the application
+	/**
+	 * The entry point of the application.
+	 *
+	 * @param args Command-line arguments (not used in this application).
+	 */
 	public static void main(String[] args) {
-		String inputFileName = "../symptoms.txt"; // Input
-													// file
-													// name
-		String outputFileName = "result.out"; // Output file name
+		String inputFileName = "../symptoms.txt";
+		String outputFileName = "result.out";
 
 		// Create an instance of AnalyticsCounter with the appropriate reader and writer
 		AnalyticsCounter counter = new AnalyticsCounter(new ReadSymptomDataFromFile(inputFileName),
 				new WriteSymptomDataToFile(outputFileName));
-		counter.generateReport(); // Generate and write the symptom report
+		counter.generateReport();
 	}
 }
